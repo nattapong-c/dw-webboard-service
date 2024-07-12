0e79e21f374dd4fc27a64a246de34c956174b015
Nattapong <nattapong@bizcuitsolution.com>
2024-07-12 20:58:23 +0700

    add test user controller

---
+++ src/infrastructure/user.repository.mock.ts
@@ -0,0 +1,40 @@
+import { HttpException, HttpStatus, Logger } from "@nestjs/common";
+import { Collection } from "mongodb";
+
+import { User, UserModel } from "src/domain/model/user";
+import { UserRepositoryInterface } from "src/domain/ports/outbound/user.repository";
+import { MongoDB } from "../utils";
+
+export class UserMockRepository implements UserRepositoryInterface {
+    private USER_DB = 'user_mock';
+    private db: Collection;
+    private logger = new Logger(UserMockRepository.name);
+
+    async create(data: User): Promise<void> {
+        try {
+            this.db = await MongoDB.connection(this.USER_DB)
+            const date = new Date();
+            await this.db.insertOne({
+                username: data.username,
+                picture: data.picture,
+                created_at: date,
+                updated_at: date
+            })
+        } catch (error) {
+            this.logger.error(error);
+            throw new HttpException('insert user error', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async get(username: string): Promise<UserModel> {
+        try {
+            this.db = await MongoDB.connection(this.USER_DB)
+            const result = await this.db.findOne<UserModel>({ username });
+            return result;
+        } catch (error) {
+            this.logger.error(error);
+            throw new HttpException('get user error', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
---
+++ src/interface/user.controller.spec.ts
@@ -0,0 +1,63 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+
+import { UserController } from './user.controller';
+import { UserService } from '../application/user.service';
+import { UserRepositoryInterface } from '../domain/ports/outbound/user.repository';
+import { UserMockRepository } from '../infrastructure/user.repository.mock';
+import { MongoDB } from '../utils';
+
+
+describe('UserController', () => {
+    const USER_DB = 'user_mock'
+    let appController: UserController;
+
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            imports: [ConfigModule.forRoot({
+                envFilePath: '.env.test'
+            })],
+            controllers: [UserController],
+            providers: [UserService,
+                {
+                    provide: UserRepositoryInterface,
+                    useClass: UserMockRepository
+                }
+            ],
+        }).compile();
+
+        appController = app.get<UserController>(UserController);
+    });
+
+    describe('create user', () => {
+        it('should return "create user success"', async () => {
+            expect(await appController.create({
+                username: 'user1',
+                picture: 'picture url'
+            })).toEqual({
+                message: 'create user success'
+            });
+        });
+
+        afterEach(async () => {
+            (await MongoDB.connection(USER_DB)).deleteMany()
+        });
+
+        it('should return "username already used"', async () => {
+            await appController.create({
+                username: 'user1'
+            });
+            try {
+                await appController.create({
+                    username: 'user1'
+                });
+            } catch (error) {
+                expect(error.message).toBe('username already used')
+            }
+        });
+
+        afterEach(async () => {
+            (await MongoDB.connection(USER_DB)).deleteMany()
+        });
+    });
+});
